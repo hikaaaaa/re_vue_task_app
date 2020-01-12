f90fd63d87c7bd51157b89ea2809f6355d6b3d8a
hikaaaaa <i.hikaru.12225@gmail.com>
2020-01-13 07:39:41 +0900

    tasks#new#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,13 @@ class TasksController < ApplicationController
   end
 
   def new
-
+    @task = Task.new
   end
 
   def create
+    @task = Task.create(task_params)
 
+    redirect_to tasks_path
   end
 
   def edit
@@ -26,4 +28,10 @@ class TasksController < ApplicationController
   def destroy
 
   end	  
-end	
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+end
